cd5ed5366b431771240cbb22c743dfaa124897bd
phillip khoza <phillipantonk@gmail.com>
2022-09-12 23:20:56 +0200

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config()
+const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express()
@@ -10,9 +11,7 @@ app.use((req,res,next)=>{
 })
 
 // route
-app.get('/',(req, res)=>{
-    res.json({message:'welcone to the app!!'})
-})
+app.use('/api/workouts',workoutRoutes)
 // listen to port request
 app.listen(process.env.PORT, ()=>{
     console.log('listening on port 5000');
