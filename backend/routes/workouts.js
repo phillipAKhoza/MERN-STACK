29cc05c9277146ae197c0c89519a723227ef1e06
phillip khoza <phillipantonk@gmail.com>
2022-09-12 23:39:18 +0200

    CRUD routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -9,6 +9,22 @@ router.get('/',(req,res)=>{
 // get single workout
 router.get('/:id',(res,req) => {
     res.json({message:'get a single workout'})
-})
+});
+
+// POST new workout
+router.post('/',(res,req) => {
+    res.json({message:'post a new workout'})
+});
+
+// DELETE a workout
+router.delete('/:id',(res,req) => {
+    res.json({message:'DELETE workout'})
+});
+
+// UPDATE a workout
+router.patch('/:id',(res,req) => {
+    res.json({message:'UPDATE workout'})
+});
+
 
 module.exports = router
