b2cf96c5b30bac7b01b3d7068e3c1248317ff631
phillip khoza <phillipantonk@gmail.com>
2022-09-12 23:46:58 +0200

    middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const workoutRoutes = require('./routes/workouts');
 // express app
 const app = express()
 
+// middleware
+app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path, req.method);
     next()
